124386b0d57fa8368ff6bec8783f712653fe8d43
lanalancia <babahgromimolniya@gmail.com>
2023-04-05 00:49:46 +0400

    Edit: chunk loading, adjusted chance

--- chunks/chunk_spawner/Player_detection_area.gd
+++ chunks/chunk_spawner/Player_detection_area.gd
@@ -2,7 +2,7 @@ extends Area3D
 
 func _on_body_entered(body):
 	if body.is_in_group("PLAYER"):
-		if randi()%9 > 0:
+		if randi()%9 > 6:
 			print(get_parent().name)
 			get_parent().lock_backtrack()
 		for n in get_parent().get_node("Attach_points").get_children():
